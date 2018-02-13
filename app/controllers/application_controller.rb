b21831b35a4b15ec70c7ce651e9aafd8f56cd37e
Arnaud <arnaud.meneroud@gmail.com>
2018-02-13 23:16:04 +0000

    safety

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
+  
+
+
  def sanitize_devise_params
  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
  end
@@ -10,6 +13,19 @@ class ApplicationController < ActionController::Base
 def hello
 	render html: "hello"
 end
+
+before_action :require_login
+
+  private
+
+  def require_login
+    unless @logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_user_registration_path # halts request cycle
+    end
+
+  end
 =end
+
 
 end
